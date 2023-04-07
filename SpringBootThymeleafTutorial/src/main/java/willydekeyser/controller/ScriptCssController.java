c45eaa45cb3fdce1e963f24a5c6d6a784a816523
willydekeyser <ma074172@skynet.be>
2023-04-07 20:14:43 +0200

    Update Script & Css

---
+++ SpringBootThymeleafTutorial/src/main/java/willydekeyser/controller/ScriptCssController.java
@@ -0,0 +1,32 @@
+package willydekeyser.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ScriptCssController {
+
+	
+	@GetMapping("/css")
+	public String home(Model model) {
+		String color = "green";
+		model.addAttribute("pageTitle", "Spring Boot Tutorial.");
+		model.addAttribute("color", color);
+		model.addAttribute("link", "/");
+		model.addAttribute("linktext", "Home");
+		return "css";
+	}
+	
+	@GetMapping("/script")
+	public String script(Model model) {
+		model.addAttribute("pageTitle", "Spring Boot Tutorial.");
+		model.addAttribute("text", "The button was clicked!");
+		model.addAttribute("link", "/");
+		model.addAttribute("linktext", "Home");
+		return "script";
+	}
+}
